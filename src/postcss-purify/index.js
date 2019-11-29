a4e93d2c68c76454d49d71e70943d0f33ab8179f
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-11-29 16:03:07 -0500

    feat: css sanitisation

--- src/postcss-purify/index.js
+++ src/postcss-purify/index.js
@@ -1,26 +1,19 @@
-const postcss = require('postcss')
-const values = require('postcss-value-parser');
+const postcss = require("postcss");
+const values = require("postcss-value-parser");
 const testProp = require("../allowed-css-props.js");
-const validDataUrl = require('valid-data-url')
+const validDataUrl = require("valid-data-url");
 
 // Some of the ideas here are from https://github.com/eramdam/postcss-sanitize/blob/master/index.js MIT License
 
-const startParenthesis = /^\(/;
-const endParenthesis = /\)$/;
-
-const deleteEmptyRules = (rule) => {
-  if (rule.nodes.length === 0)
-    rule.remove();
+const deleteEmptyRules = rule => {
+  if (rule.nodes.length === 0) rule.remove();
 };
 
-module.exports = postcss.plugin('postcss-purify', ({base}) => {
+module.exports = postcss.plugin("postcss-purify", ({ base }) => {
   return (css, result) => {
-
-    css.walkAtRules('import', (rule) => {
-      const url = rule.params.replace(startParenthesis, '').replace(endParenthesis, '');
-      if (!testPath(url, base)) {
-        rule.remove();
-      }
+    // We don't support @import rules at the moment. May do so later.
+    css.walkAtRules("import", rule => {
+      rule.remove();
     });
 
     css.walkDecls(decl => {
@@ -29,26 +22,37 @@ module.exports = postcss.plugin('postcss-purify', ({base}) => {
       }
 
       const parsed = values(decl.value);
-      parsed.walk((node) => {
-        if (node.type === 'function' && node.value === 'url') {
-          node.nodes.forEach((urlNode) => {
-            if (!testPath(urlNode.value, base) || !validDataUrl(urlNode.value)) {
-              urlNode.value = '';
-            }
-          });
+      parsed.walk(node => {
+        if (node.type === "function" && node.value === "url") {
+          node.nodes = node.nodes
+            .map(urlNode => {
+              if (
+                !testPath(urlNode.value, base) &&
+                !validDataUrl(urlNode.value)
+              ) {
+                return null;
+              } else {
+                return urlNode;
+              }
+            })
+            .filter(item => item);
         }
       });
-
-      decl.value = parsed.toString();
+      const result = parsed.toString();
+      if (result === "url()") {
+        decl.remove();
+      } else {
+        decl.value = result;
+      }
     });
 
     css.walkRules(rule => deleteEmptyRules(rule));
-  }
-})
+  };
+});
 
-function testPath (path, resourceURL) {
-  const base = new URL(resourceURL, "http://example.com")
-  const url = new URL(path, base)
+function testPath(path, resourceURL) {
+  const base = new URL(resourceURL, "http://example.com");
+  const url = new URL(path, base);
   // If the hostname doesn't equal that of the base URL we provided, then it is a full URL and so not supported
-  return url.hostname === base.hostname
+  return url.hostname === base.hostname;
 }
--- src/postcss.js
+++ src/postcss.js
@@ -1,25 +1,33 @@
-const postcss = require('postcss')
-const prefixer = require('postcss-prefix-selector')
-const selectorParser = require('postcss-selector-parser')
-function processor (root) {
-  root.walkTags((tagNode) => {
-    if (tagNode.value === 'body') {
-      tagNode.replaceWith(selectorParser.tag({value: 'ink-body'}))
-    } else if (tagNode.value === 'html') {
-      tagNode.replaceWith(selectorParser.tag({value: 'ink-html'}))
+const postcss = require("postcss");
+const purify = require("./postcss-purify");
+const prefixer = require("postcss-prefix-selector");
+const selectorParser = require("postcss-selector-parser");
+function processor(root) {
+  root.walkTags(tagNode => {
+    if (tagNode.value === "body") {
+      tagNode.replaceWith(selectorParser.tag({ value: "ink-body" }));
+    } else if (tagNode.value === "html") {
+      tagNode.replaceWith(selectorParser.tag({ value: "ink-html" }));
     }
   });
 }
 const selectorProcessor = selectorParser(processor);
-const replaceRootsWithCustomElements = postcss.plugin('classValidator', (options) => {
-    return (root) => {
-        root.walkRules(rule => {
-            selectorProcessor.processSync(rule);
-        });
+const replaceRootsWithCustomElements = postcss.plugin(
+  "classValidator",
+  options => {
+    return root => {
+      root.walkRules(rule => {
+        rule.selector = selectorProcessor.processSync(rule.selector);
+      });
     };
-});
+  }
+);
 
-module.exports = async function processCSS (css, base, {prefix = '#ink-engine'}) {
-  const result = await postcss([prefixer({prefix}), replaceRootsWithCustomElements()]).process(css)
-  return result.css
-}
+module.exports = async function processCSS(css, base, prefix = "#ink-engine") {
+  const result = await postcss([
+    purify({ base }),
+    prefixer({ prefix }),
+    replaceRootsWithCustomElements()
+  ]).process(css, { from: base, to: base });
+  return result.css;
+};
