7191f450f6a3eb68912669bf974e8c6eeb2cb09b
Baldur Bjarnason <bb@baldurbjarnason.com>
2019-12-02 14:37:05 -0500

    feat: dompurify tests

--- src/dompurify.js
+++ src/dompurify.js
@@ -1,95 +1,63 @@
-const createDOMPurify = require('dompurify');
-const { JSDOM } = require('jsdom');
-const testProp = require("./allowed-css-props.js");
-const validDataUrl = require('valid-data-url')
+const createDOMPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const validDataUrl = require("valid-data-url");
+const processCSS = require("./postcss");
 
 const purifyConfig = {
   KEEP_CONTENT: false,
   IN_PLACE: true,
   WHOLE_DOCUMENT: true,
-  ALLOW_TAGS: ["reader-highlight"],
-  FORBID_TAGS: ["meta", "form", "title"],
+  ADD_TAGS: ["link"],
+  FORBID_TAGS: ["meta", "form"],
   FORBID_ATTR: ["srcset", "action", "background", "poster"]
 };
 
 // In theory this should work for SVG images as well.
-module.exports = function purifyChapter(
+module.exports = async function purifyPreprocess(
   chapter,
   chapterPath,
   contentType = "text/html"
 ) {
-  const resourceURL = new URL(chapterPath, 'https://example.com/');
+  const resourceURL = new URL(chapterPath, "https://example.com/");
   let dom;
   try {
     dom = new JSDOM(chapter, {
       contentType
     });
   } catch (err) {
-    // console.log(err);
     dom = new JSDOM(chapter, {
       contentType: "text/html"
     });
   }
   const window = dom.window;
-  const clean = purify(resourceURL, window)
-  return dom.serialize(clean)
-}
+  for (const node of window.document.querySelectorAll("[style]")) {
+    try {
+      const styles = await processCSS(
+        `body {${node.getAttribute("style")}}`,
+        resourceURL.href
+      );
+      node.setAttribute("style", styles.split(/{|}/)[1]);
+    } catch (err) {
+      node.removeAttribute("style");
+    }
+  }
+  for (const node of window.document.querySelectorAll("style")) {
+    try {
+      const styles = await processCSS(node.textContent, resourceURL.href);
+      node.textContent = styles;
+    } catch (err) {
+      node.textContent = "";
+    }
+  }
+  const clean = purify(resourceURL, window);
+  return dom.serialize(clean);
+};
 
-function purify (resourceURL, window) {
+function purify(resourceURL, window) {
   const DOMPurify = createDOMPurify(window);
   // Based on sample from https://github.com/cure53/DOMPurify/tree/master/demos, same license as DOMPurify
 
-  function processURL(prop) {
-    const href = /url\("?([^)|"]+)(?!data:)/gim.exec(prop)[1];
-    const url = new URL(href, resourceURL);
-    if (
-      url.host === resourceURL.host &&
-      url.protocol === resourceURL.protocol
-    ) {
-      return `url("${href}")`;
-    } else {
-      return null;
-    }
-  }
-
-  function addStyles(output, styles) {
-    for (var prop = styles.length - 1; prop >= 0; prop--) {
-      const regex = /url\("?([^)|"]+)(?!data:)/gim;
-      if (styles[styles[prop]] && regex.test(styles[styles[prop]])) {
-        var url = processURL(styles[styles[prop]]);
-        styles[styles[prop]] = url;
-      }
-      if (
-        styles[styles[prop]] &&
-        typeof styles[styles[prop]] === "string" &&
-        testProp(styles[prop])
-      ) {
-        output.push(styles[prop] + ":" + styles[styles[prop]] + ";");
-      }
-    }
-  }
-
-  function addCSSRules(output, cssRules) {
-    for (var index = cssRules.length - 1; index >= 0; index--) {
-      var rule = cssRules[index];
-      // check for rules with selector
-      if (rule.type === 1 && rule.selectorText) {
-        output.push(rule.selectorText + "{");
-        if (rule.style) {
-          addStyles(output, rule.style);
-        }
-        output.push("}");
-        // check for @media rules
-      }
-    }
-  }
-
   DOMPurify.addHook("uponSanitizeElement", function(node, data) {
-    if (data.tagName === "style") {
-      var output = [];
-      addCSSRules(output, node.sheet.cssRules);
-      node.textContent = output.join("\n");
-    }
     if (
       node.getAttributeNS &&
       node.getAttributeNS("http://www.idpf.org/2007/ops", "type")
@@ -101,44 +69,30 @@ function purify (resourceURL, window) {
     }
   });
   DOMPurify.addHook("afterSanitizeAttributes", function(node) {
-    if (node.hasAttribute("style")) {
-      var styles = node.style;
-      var output = [];
-      for (var prop = styles.length - 1; prop >= 0; prop--) {
-        // we re-write each property-value pair to remove invalid CSS
-        const regex = /url\("?([^)|"]+)(?!data:)/gim;
-        if (node.style[styles[prop]] && regex.test(node.style[styles[prop]])) {
-          node.style[styles[prop]] = processURL(node.style[styles[prop]]);
-        }
-        if (node.style[styles[prop]]) {
-          output.push(styles[prop] + ":" + node.style[styles[prop]] + ";");
-        }
-      }
-      // re-add styles in case any are left
-      if (output.length) {
-        node.setAttribute("style", output.join(""));
-      } else {
-        node.removeAttribute("style");
-      }
-    }
     // All src urls must be relative. This will have to be improved once we start expanding our format support
-    if (node.hasAttribute("src") && (!testPath(node.getAttribute("src"), resourceURL) || validDataUrl(node.getAttribute("src")))) {
-      node.removeAttribute("src")
+    if (
+      node.hasAttribute("src") &&
+      !testPath(node.getAttribute("src"), resourceURL) &&
+      !validDataUrl(node.getAttribute("src"))
+    ) {
+      node.remove();
     }
-    if (node.hasAttribute("href") && !testPath(node.getAttribute("href"), resourceURL)) {
-      node.removeAttribute("href")
+    if (
+      node.tagName.toLowerCase() === "link" &&
+      node.hasAttribute("href") &&
+      node.getAttribute("rel") === "stylesheet" &&
+      !testPath(node.getAttribute("href"), resourceURL)
+    ) {
+      console.log("removing node");
+      node.remove();
     }
   });
-  return DOMPurify.sanitize(
-    window.document.documentElement,
-    purifyConfig
-  );
-
+  return DOMPurify.sanitize(window.document.documentElement, purifyConfig);
 }
 
-function testPath (path, resourceURL) {
-  const base = new URL(resourceURL, "http://example.com")
-  const url = new URL(path, base)
+function testPath(path, resourceURL) {
+  const base = new URL(resourceURL, "http://example.com");
+  const url = new URL(path, base);
   // If the hostname doesn't equal that of the base URL we provided, then it is a full URL and so not supported
-  return url.hostname === base.hostname
+  return url.hostname === base.hostname;
 }
