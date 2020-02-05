0940e7db85bd8ec6dd59442591710dd77dd5f4b8
Baldur Bjarnason <bb@baldurbjarnason.com>
2020-02-05 13:19:13 -0500

    fix: turning off keywords and wordcount

--- src/unified/index.js
+++ src/unified/index.js
@@ -2,25 +2,25 @@ const engine = require("unified-engine");
 const unified = require("unified");
 const parse = require("./parse");
 // const stringify = require("rehype-stringify");
-const text = require("./rehype-text");
-const latin = require("retext-latin");
-const wordcount = require("./wordcount");
+// const text = require("./rehype-text");
+// const latin = require("retext-latin");
+// const wordcount = require("./wordcount");
 const transformer = require("./transformer");
 const slug = require("rehype-slug");
-const pos = require("retext-pos");
-const keywords = require("retext-keywords");
+// const pos = require("retext-pos");
+// const keywords = require("retext-keywords");
 // const fs = require("fs");
 
 const processor = unified()
   .use(parse)
-  .use(
-    text,
-    unified()
-      .use(latin)
-      .use(pos)
-      .use(keywords)
-      .use(wordcount)
-  )
+  // .use(
+  //   text,
+  //   unified()
+  //     .use(latin)
+  //     // .use(pos)
+  //     // .use(keywords)
+  //     .use(wordcount)
+  // )
   .use(slug)
   .use(transformer)
   .freeze();
@@ -33,11 +33,15 @@ module.exports = function processEngine({ files, output, cwd }, extract) {
         files,
         cwd,
         output,
-        reporter: 'silent',
+        silent: true,
         treeOut: true
       },
       function(err, code, context) {
-        if (err || code === 1) return reject(new Error("Processing failed"));
+        if (code === 1) {
+          return reject(new Error("Processing failed"))
+        } else if (err) {
+          return reject(err)
+        };
         return processFiles(files, extract).then(wordcount => {
           context.wordcount = wordcount;
           return resolve(context);
