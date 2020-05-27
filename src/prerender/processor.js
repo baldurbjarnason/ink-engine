8a2cee6c0d954a27dfaea54d04f9911e311c6c73
Baldur Bjarnason <baldur@rebus.foundation>
2020-05-26 23:39:22 -0400

    Basic annotations integration

--- src/prerender/processor.js
+++ src/prerender/processor.js
@@ -1,9 +1,12 @@
 const unified = require("unified");
 const transformer = require("./transformer");
 const html = require("rehype-stringify");
+const annotate = require("rehype-annotate");
 
 module.exports = function getProcessor(options) {
+  const { annotations = [], documentURL } = options;
   return unified()
+    .use(annotate, { annotations, url: documentURL, stimulus: true })
     .use(transformer, options)
     .use(html);
 };
