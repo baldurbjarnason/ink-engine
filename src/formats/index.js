d8f535e1a99e4b2f9fdad58494ae920dfdbaa114
Baldur Bjarnason <baldur@rebus.foundation>
2020-08-26 15:54:13 +0000

    Added support for processing HTML

--- src/formats/index.js
+++ src/formats/index.js
@@ -1,9 +1,12 @@
 const docx = require("./DocxProcess");
 const epub = require("./EpubProcess");
+const markup = require("./MarkupProcess");
 
 module.exports = {
   "application/epub+zip": epub,
   epub: epub,
   "application/vnd.openxmlformats-officedocument.wordprocessingml.document": docx,
-  docx: docx
+  docx: docx,
+  "text/html": markup,
+  html: markup
 };
