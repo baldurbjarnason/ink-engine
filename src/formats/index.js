01e48a691cc21756a7bf543b61db4ed481e4d7d2
Baldur Bjarnason <baldur@rebus.foundation>
2020-09-03 17:45:15 +0000

    Simple format process funciton.

--- src/formats/index.js
+++ src/formats/index.js
@@ -1,12 +1,32 @@
-const docx = require("./DocxProcess");
-const epub = require("./EpubProcess");
-const markup = require("./MarkupProcess");
+const docx = require("./docx");
+const epub = require("./epub");
+const markup = require("./markup");
+const pdf = require("./pdf");
 
-module.exports = {
-  "application/epub+zip": epub,
-  epub: epub,
-  "application/vnd.openxmlformats-officedocument.wordprocessingml.document": docx,
-  docx: docx,
-  "text/html": markup,
-  html: markup
-};
+async function* process (mediaType, options) {
+  let processor
+  switch (mediaType) {
+    case "epub":
+    case "application/epub+zip":
+      processor = epub
+      break;
+    case "docx":
+    case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+      processor = docx
+      break;
+    case "html":
+    case "text/html":
+      processor = markup
+      break;
+    case "pdf":
+    case "application/pdf":
+      processor = pdf
+      break;
+    default:
+      processor = pdf
+      break;
+  }
+  yield* processor(options)
+}
+
+module.exports = process
